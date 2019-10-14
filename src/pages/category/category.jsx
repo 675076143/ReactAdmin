2f388e0fa1ae4c5107d24028fe252fb17149853b
RobinJohn <robin675076143@hotmail.com>
2019-10-14 21:41:13 +0800

    完善二级分类头部显示效果

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -9,6 +9,8 @@ import {reqLogin, reqTopCategory,reqSecondaryCategory} from "../../api";
 export default class Category extends Component{
 
     state = {
+        categoryLevel:1,
+        parentName:'',
         rowKey:'topCategoryID',
         loading:false,
         categories:[], //一级分类列表
@@ -24,7 +26,7 @@ export default class Category extends Component{
                 render: (category) => (
                     <span>
                         <a>修改分类</a>
-                        <a style={{margin:"20px"}} onClick={()=>{this.getSecondaryCategories(category.topCategoryID)}}>查看子分类</a>
+                        <a style={{margin:"20px"}} onClick={()=>{this.getSecondaryCategories(category.topCategoryID, category.topCategoryName)}}>查看子分类</a>
                     </span>
                 )
             },
@@ -36,9 +38,14 @@ export default class Category extends Component{
         this.setState({loading:true})
         const  result = await reqTopCategory();
         if(result.code == 200){
+            //更换列的dataIndex和key
+            this.initColumns('topCategoryName','topCategoryID')
             const categories = result.data;
+            //设置状态
             this.setState({
-                categories
+                categories,
+                rowKey:'topCategoryID',
+                categoryLevel:1
             })
         }else {
             message.error('获取分类失败!')
@@ -48,16 +55,19 @@ export default class Category extends Component{
 
     }
     //获取二级列表
-    getSecondaryCategories = async (topCategoryID) =>{
-        console.log(topCategoryID)
+    getSecondaryCategories = async (topCategoryID, topCategoryName) =>{
+        console.log(topCategoryID,topCategoryName)
         const result = await reqSecondaryCategory(topCategoryID)
         if(result.code == 200){
             //更换列的dataIndex和key
             this.initColumns('secondaryCategoryName','secondaryCategoryID')
             const categories = result.data;
+            //设置状态
             this.setState({
                 categories,
-                rowKey:'secondaryCategoryID'
+                rowKey:'secondaryCategoryID',
+                categoryLevel:2,
+                parentName:topCategoryName
             })
         }else {
             message.error('获取分类失败!')
@@ -75,8 +85,14 @@ export default class Category extends Component{
 
     render(){
         //读取状态数据
-        const {categories,loading,rowKey} = this.state
-        const title = '一级分类列表'
+        const {categoryLevel,parentName,categories,loading,rowKey} = this.state
+        const title = categoryLevel===1?'一级分类列表':(
+            <span>
+                <a onClick={this.getTopCategories}>一级分类列表</a>
+                <Icon type='arrow-right' style={{marginRight:5,marginLeft:5}}/>
+                <span>{parentName}</span>
+            </span>
+        )
         const extra = (
             <Button type="primary">
                 <Icon type="plus" />
