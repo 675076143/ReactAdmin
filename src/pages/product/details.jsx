3a03f60b4c43948316f68dcfd5a35de0e40b4419
RobinJohn <robin675076143@hotmail.com>
2019-10-15 22:54:06 +0800

    商品页面路由搭建

---
+++ src/pages/product/details.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {Redirect, Route, Switch} from "react-router-dom";
+/*
+* 主页
+* */
+
+export default class ProductDetails extends Component{
+    render(){
+        return (
+            <div>
+                Details
+            </div>
+        )
+    }
+}
---
+++ src/pages/product/home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {Redirect, Route, Switch} from "react-router-dom";
+/*
+* 主页
+* */
+
+export default class ProductHome extends Component{
+    render(){
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
