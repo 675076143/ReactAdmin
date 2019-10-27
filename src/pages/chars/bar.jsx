03ad7e50370e08cea2eafa2f07f8f04794c58ed6
RobinJohn <robin675076143@hotmail.com>
2019-10-28 02:31:33 +0800

    首页模块

--- src/pages/chars/bar.jsx
+++ src/pages/chars/bar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Card} from "antd";
+import {Button, Card, Icon} from "antd";
 import ReactEcharts from "echarts-for-react";
 
 export default class Bar extends Component{
@@ -9,29 +9,90 @@ export default class Bar extends Component{
      */
     getOption = ()=>{
         return {
-            title: {
-                text: 'ECharts 入门示例'
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                    type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
             },
-            tooltip: {},
             legend: {
-                data:['销量']
+                data:['利润', '支出', '收入']
             },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
             },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
+            xAxis : [
+                {
+                    type : 'value'
+                }
+            ],
+            yAxis : [
+                {
+                    type : 'category',
+                    axisTick : {show: false},
+                    data : ['周一','周二','周三','周四','周五','周六','周日']
+                }
+            ],
+            series : [
+                {
+                    name:'利润',
+                    type:'bar',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'inside'
+                        }
+                    },
+                    data:[200, 170, 240, 244, 200, 220, 210]
+                },
+                {
+                    name:'收入',
+                    type:'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true
+                        }
+                    },
+                    data:[320, 302, 341, 374, 390, 450, 420]
+                },
+                {
+                    name:'支出',
+                    type:'bar',
+                    stack: '总量',
+                    label: {
+                        normal: {
+                            show: true,
+                            position: 'left'
+                        }
+                    },
+                    data:[-120, -132, -101, -134, -190, -230, -210]
+                }
+            ]
         }
     }
 
+
     render(){
+
+        const title = (
+            <span>
+                <Button type='primary'>
+                    <Icon type="redo" />
+                    更新
+                </Button>
+            </span>
+        )
+
         return (
-            <Card>
-                <ReactEcharts option={this.getOption()}/>
+            <Card title={title}>
+                <ReactEcharts
+                    option={this.getOption()}
+                    theme='chalk'
+                />
             </Card>
         )
     }
