12d72d8d1a508a5640d3cb2a545828db00024ace
RobinJohn <robin675076143@hotmail.com>
2019-10-28 00:43:07 +0800

    使用echarts制作简单柱状图

--- src/pages/chars/bar.jsx
+++ src/pages/chars/bar.jsx
@@ -1,11 +1,38 @@
 import React, {Component} from "react";
+import {Card} from "antd";
+import ReactEcharts from "echarts-for-react";
 
 export default class Bar extends Component{
+
+    /*
+    返回柱状图的配置对象
+     */
+    getOption = ()=>{
+        return {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        }
+    }
+
     render(){
         return (
-            <div>
-
-            </div>
+            <Card>
+                <ReactEcharts option={this.getOption()}/>
+            </Card>
         )
     }
 }
